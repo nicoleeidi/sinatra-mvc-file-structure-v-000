7f93b9aa0e4e788ea67b1d4880562257819e6933
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-12 18:55:22 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,10 @@
-class Dog 
-  attr_accessor :name, :breed, :age 
-end 
+class Dog
+  attr_accessor :name, :breed, :age
+  @@dogs = []
+  def initialize(name,breed,age)
+    @name=name
+    @breed=breed
+    @age=age
+    @@dogs << self
+  end
+end
